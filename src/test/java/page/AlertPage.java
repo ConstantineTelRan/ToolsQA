e209bb77234e189fb6bba5ed5603be0003da9f20
coss <cossx@yahoo.com>
2023-04-18 02:07:10 +0600

    add alert

---
+++ src/test/java/page/AlertPage.java
@@ -0,0 +1,32 @@
+package page;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AlertPage extends PageBase{
+    public AlertPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//button[@id=\"timerAlertButton\"]")
+    private WebElement timerAlertButton;
+
+    public void clickToTimerAlertButton() {
+        timerAlertButton.click();
+        new WebDriverWait(driver, Duration.ofSeconds(6))
+                .until(ExpectedConditions.alertIsPresent());
+    }
+
+    public String getAlertText() {
+        Alert alert = driver.switchTo().alert();
+        return alert.getText();
+    }
+
+
+}
---
+++ src/test/java/tests/AlertTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.AlertPage;
+
+public class AlertTest extends TestBase {
+    AlertPage alertPage;
+    String alertExpectedText = "This alert appeared after 5 seconds";
+    String alertActualText;
+    String url = "https://demoqa.com/alerts";
+
+    @BeforeMethod
+    public void openUrl() {
+        driver.get(url);
+    }
+
+    @Test
+    public void timerAlertTest() {
+        alertPage = new AlertPage(driver);
+        alertPage.clickToTimerAlertButton();
+        alertActualText = alertPage.getAlertText();
+        Assert.assertEquals(alertActualText, alertExpectedText);
+    }
+
+}
