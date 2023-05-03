ab261eebd46e4894a5e412055092b1019d6dfd15
coss <cossx@yahoo.com>
2023-05-04 00:42:58 +0600

    add dragdrop99

---
+++ src/test/java/page/DragAndDropPage.java
@@ -0,0 +1,48 @@
+package page;
+
+
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class DragAndDropPage extends PageBase{
+    public DragAndDropPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@id=\"draggable\"]")
+    public WebElement draggable;
+
+    @FindBy(xpath = "//div[@id=\"droppable\"]")
+    public WebElement droppable;
+
+    @FindBy(xpath = "//a[@id=\"droppableExample-tab-revertable\"]")
+    public WebElement revertDraggableTab;
+
+    @FindBy(xpath = "//div[@id=\"revertable\"]")
+    public WebElement revertable;
+
+    @FindBy(xpath = "//div[@id=\"simpleDropContainer\"]//div[@id=\"droppable\"]")
+    public WebElement simpleDropContainer;
+
+
+    public void dragAndDrop(WebElement elementDraggable, WebElement elementDroppable) {
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(elementDraggable, elementDroppable).perform();
+    }
+
+    public String getElementText(WebElement element) {
+        return element.getText();
+    }
+
+    public Point getLoc(WebElement element) {
+        return element.getLocation();
+    }
+
+    public void clickTo(WebElement element) {
+        element.click();
+    }
+
+}
---
+++ src/test/java/tests/DragAndDropTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.openqa.selenium.Point;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.DragAndDropPage;
+
+public class DragAndDropTest extends TestBase {
+    DragAndDropPage dragAndDropPage;
+    String url = "https://demoqa.com/droppable";
+
+    @BeforeMethod
+    public void openUrl() {
+        driver.get(url);
+    }
+
+    @Test
+    public void dragAndDropTest() {
+        dragAndDropPage = new DragAndDropPage(driver);
+        Point startLoc = dragAndDropPage.getLoc(dragAndDropPage.draggable);
+        dragAndDropPage.dragAndDrop(dragAndDropPage.draggable, dragAndDropPage.droppable);
+        String actualText = dragAndDropPage.getElementText(dragAndDropPage.droppable);
+        Point finishLoc = dragAndDropPage.getLoc(dragAndDropPage.draggable);
+        Assert.assertEquals(actualText, "Dropped!");
+        Assert.assertNotEquals(startLoc, finishLoc);
+    }
+
+
+    @Test
+    public void revertDDTest() {
+        dragAndDropPage = new DragAndDropPage(driver);
+        dragAndDropPage.clickTo(dragAndDropPage.revertDraggableTab);
+        Point startLoc = dragAndDropPage.getLoc(dragAndDropPage.revertable);
+        dragAndDropPage.dragAndDrop(dragAndDropPage.revertable, dragAndDropPage.simpleDropContainer);
+        Point finishLoc = dragAndDropPage.getLoc(dragAndDropPage.revertable);
+        Assert.assertEquals(startLoc, finishLoc);
+    }
+
+}
